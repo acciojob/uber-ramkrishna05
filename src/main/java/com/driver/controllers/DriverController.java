11ed439b73600ac17fc038d115f58ef9be3b2fd2
ramkrishna_05 <ramkrishnapatel306@gmail.com>
2023-12-25 16:32:34 +0530

    five

--- src/main/java/com/driver/controllers/DriverController.java
+++ src/main/java/com/driver/controllers/DriverController.java
@@ -11,23 +11,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/driver")
 public class DriverController {
+
 	@Autowired
-private DriverServiceImpl driverService;
-	
+	DriverServiceImpl driverServiceImpl;
+
 	@PostMapping(value = "/register")
 	public ResponseEntity<Void> registerDriver(@RequestParam String mobile, @RequestParam String password){
-		driverService.register(mobile,password);
-
+		driverServiceImpl.register(mobile,password);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping(value = "/delete")
 	public void deleteDriver(@RequestParam Integer driverId){
-		driverService.removeDriver(driverId);
+		driverServiceImpl.removeDriver(driverId);
 	}
 
 	@PutMapping("/status")
 	public void updateStatus(@RequestParam Integer driverId){
-		driverService.updateStatus(driverId);
+		driverServiceImpl.updateStatus(driverId);
 	}
 }
